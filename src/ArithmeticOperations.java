e14fe70248c4284fd669b2b8096d3675deb73c46
Mohamed Mujeeb <89663581+Mujeeb351@users.noreply.github.com>
2026-02-13 22:45:20 +0530

    first commit and setup

---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,39 @@
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        // Arithmetic Operations
+
+        /*
+           1. +
+           2. -
+           3. *
+           4. /
+           5. %
+
+         */
+
+        int a = 50;
+        int b = 20;
+        int total;
+        int sub;
+        int multi;
+        int div;
+        int mod;
+
+        total = a + b;
+        System.out.println("The total of a + b = " + total);
+
+        sub = a - b;
+        System.out.println("The Subtraction of a - b = " + sub);
+
+        multi = a * b;
+        System.out.println("The Multiplication of a * b = " + multi);
+
+        div = a / b;
+        System.out.println("The Division of a / b = " + div);
+
+        mod = a % b;
+        System.out.println("The Modulus of a % b = " + mod);
+
+    }
+}
---
+++ src/DataTypes.java
@@ -0,0 +1,39 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // booleans (True/False)
+        boolean bool = true; // Variable 1. Data type  2. Variable name  3. value
+        System.out.println(bool); // true
+
+        // Integers (100, 12365, 14789654)
+        byte by = 100;
+        System.out.println(by);
+
+        short srt = 1100;
+        System.out.println(srt);
+
+        int in = 1000000;
+        System.out.println(in);
+
+        long ln = 1698745689552L;
+        //long ln2; 0
+        System.out.println(ln);
+
+        // fractional numbers (123.321, 1478965.258741)
+        float fl = 12.32F;
+        System.out.println(fl);
+
+        double db = 12365.25874;
+        System.out.println(db);
+
+        // characters (A , B, #, &, a , b)
+        char ch = 'A';
+        System.out.println(ch);
+
+        // Combination of characters (Words, BCAS, Sainthamaruthu)
+        String str = "BCAS";
+        System.out.println(str);
+
+    }
+}
